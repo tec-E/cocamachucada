a95d7d00378a078e79dd2a55ef0174841ae6141a
tec-E <teco92justo@gmail.com>
2025-07-16 14:35:16 -0400

    Ruta de prueba para verificar conexión con base de datos

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/productos', function () {
     $productos = Producto::with('categoria')->where('cantidad', '>', 0)->get();
     return view('productos.index', compact('productos'));
 });
+Route::get('/probar-db', function () {
+    try {
+        $tablas = DB::select('SHOW TABLES');
+        return response()->json($tablas);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()]);
+    }
+});
