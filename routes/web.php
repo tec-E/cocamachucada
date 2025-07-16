8817388d9c72f2e0b02b7de00e2dd6940c7398fb
tec-E <teco92justo@gmail.com>
2025-07-16 13:45:34 -0400

    Redirigir ruta raíz / al login de Filament /admin/login

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Producto;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin/login');
+
 Route::get('/productos', function () {
     $productos = Producto::with('categoria')->where('cantidad', '>', 0)->get();
     return view('productos.index', compact('productos'));
