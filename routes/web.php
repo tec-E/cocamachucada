c11a7515a89c15f154c4a6e1297b3d5e936a1133
tec-E <teco92justo@gmail.com>
2025-07-17 10:37:45 -0400

    Actualiza redirección en ruta principal

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Producto;
 
-Route::redirect('/', '/admin');
+Route::redirect('/', '/admin/login');
 
 Route::get('/productos', function () {
     $productos = Producto::with('categoria')->where('cantidad', '>', 0)->get();
